3d923b5aa2922c9ff38732a1bc5655e78dbe8fe4
TortillaChips <tortilla031@gmail.com>
2023-02-19 17:28:31 -0500

    Static users

--- Front-end/src/functions/GetThreads.js
+++ Front-end/src/functions/GetThreads.js
@@ -1,7 +1,7 @@
 const DUMMY_THREADS = [
     {
         id: 't1',
-        creator: 'u1',
+        creator: 'Jane Doe',
         subject: 'A whole essay on why you should eat an apple a day',
         description:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tristique neque nec laoreet mollis. Suspendisse ac mollis ipsum. Vestibulum volutpat vel lorem quis lobortis. Praesent suscipit euismod est at efficitur. ',
@@ -9,14 +9,14 @@ const DUMMY_THREADS = [
             {
                 id: 'a1',
                 answer: 'Literally no one gives a damn. I just ratioed your entire post. Like Oh my goodness gracious the world is goign to fall appart like I am so emo right now ok my bad im sorry for uhhhhh breaking your feelings ok bye',
-                creator: 'u3',
+                creator: 'Pickle Doe',
                 upvotes: 5186,
                 downvotes: 0,
             },
             {
                 id: 'a2',
                 answer: 'Pickle Juice?',
-                creator: 'u2',
+                creator: 'John Doe',
                 upvotes: 2,
                 downvotes: 50,
             },
@@ -24,7 +24,7 @@ const DUMMY_THREADS = [
     },
     {
         id: 't2',
-        creator: 'u2',
+        creator: 'John Doe',
         subject: 'Bananas suck and here is why',
         description:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tristique neque nec laoreet mollis. Suspendisse ac mollis ipsum. Vestibulum volutpat vel lorem quis lobortis. Praesent suscipit euismod est at efficitur. ',
